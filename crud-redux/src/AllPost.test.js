27fa404145c912fb2b0ab2a15e53a7c1bb0a652e
Karen Monsalve <alexandrakaren1992@gmail.com>
2018-10-19 14:34:49 -0400

    first commit

---
+++ crud-redux/src/AllPost.test.js
@@ -0,0 +1,37 @@
+import AllPost from '../AllPost';
+const expect = global.expect;
+
+describe('AllPost', () =>{
+    const AllPost1 = [{
+        comentario = 'maria',
+        descripcion = 'maria  es  una  chica',
+
+    }, {
+        comentario = 'pedro',
+        descripcion = 'Pedro es un chico',
+
+}];
+});
+
+describe ('Post', () => {
+    BeforeEach(() => {
+
+
+        AllPost1.reiniciar();
+    });
+
+    test('Debe mostrar los post con props ')
+
+
+
+})
+
+
+
+
+
+
+
+
+
+})
